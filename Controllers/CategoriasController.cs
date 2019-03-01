849d703fb08cba83fc7e816bd9a01149ebb532d7
Arami <aramiaracely@gmail.com>
2019-03-01 17:38:55 -0300

    model y controller de categoria terminado con validaciones incluidas

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using rest_api_sigedi.Annotations;
 using rest_api_sigedi.Models;
 
@@ -12,6 +14,21 @@ namespace rest_api_sigedi.Controllers
         public CategoriasController(DataContext context, IMapper mapper) 
         : base(context, mapper)
         {}
+
+        // evaluamos que no se repita un registro de categorias
+        protected override async Task<bool> IsValidModel(CategoriaDto dto)
+        {
+            if (await _context.Categorias
+            .AnyAsync(v => v.Descripcion == dto.Descripcion && v.Id != dto.Id))
+            {
+                ModelState.AddModelError(
+                nameof(dto.Descripcion),
+                $"Ya existe una categoria con esa descripción \"{dto.Descripcion}\" en el sistema");
+
+                return false;
+            }
+            return true;
+        }
     }
 
     public class CategoriaDto: DtoBase
